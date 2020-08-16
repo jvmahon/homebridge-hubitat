de239b9eb2790a0edd20f3f1a2ec603d98a93545
jvmahon <jvm33@columbia.edu>
2020-08-16 15:43:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = function (homebridge) {
 }
 
 var exports = module.exports;
-var globals = [];
+
 var HubitatSystem = require('./lib/HubitatSystemObject');
 var HubData = new HubitatSystem();
 var HomekitSetup = require("./lib/HomeKitDeviceSetup");
@@ -26,22 +26,10 @@ var HomekitSetup = require("./lib/HomeKitDeviceSetup");
 
 function HubitatPlatform(log, config, api) {
 
-	// if(!config) return([]);
-
 	this.log = log;
     this.config = config;
 	this.api = api;
-	// console.log("Config is: " + Object.getOwnPropertyNames(config));
-	// console.log("api.hap is: " + Object.getOwnPropertyNames(api.hap));
-	
-
-	globals.log = log; 
-	globals.platformConfig = config; // Platform variables from config.json:  platform, name, host, temperatureScale, lightbulbs, thermostats, events, accessories
-	globals.api = api; // _accessories, _platforms, _configurableAccessories, _dynamicPlatforms, version, serverVersion, user, hap, hapLegacyTypes,platformAccessory,_events, _eventsCount
-	console.log(chalk.yellow("Globals: " + Object.getOwnPropertyNames(globals)));
-
 }
-
 
 HubitatPlatform.prototype = 
 {
@@ -53,7 +41,7 @@ HubitatPlatform.prototype =
 		
 		for (var currentAccessory of HubData.allDevices) 
 		{
-			globals.log(chalk.green(`Creating new Hubitat Accessary for device ${chalk.cyan(currentAccessory.name)} with an ID number ${chalk.cyan(currentAccessory.id)}, and a type ${chalk.cyan(currentAccessory.type)}.`))
+			this.log(chalk.green(`Creating new Hubitat Accessary for device ${chalk.cyan(currentAccessory.name)} with an ID number ${chalk.cyan(currentAccessory.id)}, and a type ${chalk.cyan(currentAccessory.type)}.`))
 			
 			var accessory = new HubitatAccessory(this.api, this.log, this.config, currentAccessory, HubData);
 			
@@ -65,7 +53,6 @@ HubitatPlatform.prototype =
 	}
 }
 
-
 function HubitatAccessory(api, log, platformConfig, currentAccessory, HubInfo) {
 	this.api = api;
 	this.log = log;
@@ -73,14 +60,10 @@ function HubitatAccessory(api, log, platformConfig, currentAccessory, HubInfo) {
 	this.currentAccessory = currentAccessory;
 	this.HubData = HubInfo;    
 
-    this.config = currentAccessory;
+	// The following two parameters are mandatory. HomeBridge will throw an error if you don't specify them!
     this.name = currentAccessory.name
-    this.model = currentAccessory.model || currentAccessory.label;
-	this.manufacturer = currentAccessory.manufacturer || "Hubitat"
-	this.type = currentAccessory.type
 	this.uuid_base = currentAccessory.id;
-	this.id = currentAccessory.id;
-	this.HubData = HubInfo;	
+
 }
 
 HubitatAccessory.prototype = {
@@ -96,9 +79,3 @@ HubitatAccessory.prototype = {
         return services;
     }
 }
-
-exports.platform = HubitatPlatform;
-exports.globals = globals;
-exports.HubData = HubData;
-
-
